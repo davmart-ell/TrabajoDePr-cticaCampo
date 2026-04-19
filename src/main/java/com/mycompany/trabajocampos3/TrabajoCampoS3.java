1acc164d57bf38efd9e879c8b278404ac740b2b5
David Martell <davmarteduc78@gmail.com>
2026-04-19 17:48:01 -0500

    Primer Commit

---
+++ src/main/java/com/mycompany/trabajocampos3/TrabajoCampoS3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabajocampos3;
+
+/**
+ *
+ * @author Dav
+ */
+public class TrabajoCampoS3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
